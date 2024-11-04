15a201cc5c0a3a784aaea0fd22ffb226c2758462
Maxim Khamidulin <Maxim Khamidulin@DESKTOP-O9G6VIU>
2024-11-04 20:54:01 +0300

    Добавьте файлы проекта.

---
+++ Ex1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+Обязательная задача*. Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом.
+Знаки препинания не используются. Найти самое длинное слово в строке.
+*/
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string s = Console.ReadLine();
+
+            string[] s_new = s.Split(' ');
+
+            string s_word = s_new[0];
+            int s_max = s_new[0].Length;
+
+            foreach (string i in s_new)
+            {
+                if (i.Length > s_max)
+                {
+                    s_word = i;
+                    s_max = i.Length;
+                }
+            }
+
+            Console.WriteLine("Самое длинное слово '{0}'", s_word);
+            Console.WriteLine("Длина слова {0} символов", s_max);
+            Console.ReadKey();
+        }
+    }
+}
